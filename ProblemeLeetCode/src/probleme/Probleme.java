30a8499e9a7f515c135b01d8c04fa3055d6d4639
Badescu Oliver <olibadescu@gmail.com>
2024-07-09 12:58:08 +0300

    p21

--- ProblemeLeetCode/src/probleme/Probleme.java
+++ ProblemeLeetCode/src/probleme/Probleme.java
@@ -450,5 +450,18 @@ public class Probleme {
     }
 
 
+    //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+    public static boolean p21(int[] nums){
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
 
 }
